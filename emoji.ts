184d368f8dc6225284fa0eb43c7a4047ca2fa8bb
Saeden <matthijs.wolters@proton.me>
2023-12-11 23:27:44 +0800

    list formatting with flag

--- emoji.ts
+++ emoji.ts
@@ -9,3 +9,38 @@ export class EmojiWidget extends WidgetType {
     return div;
   }
 }
+
+export class HalfCompletedWidget extends WidgetType {
+    toDOM(view: EditorView): HTMLElement {
+        const label = document.createElement("label");
+
+        label.className = "task-list-label";
+
+        // Set contenteditable attribute to false
+        label.setAttribute("contenteditable", "false");
+
+        const input = document.createElement("input");
+
+        input.className = "task-list-item-checkbox";
+        input.type = "checkbox";
+
+        // Set data-task attribute
+        input.setAttribute("data-task", "/");
+
+        // Append the input element to the label element
+        label.appendChild(input);
+
+        return label;
+
+    }
+}
+
+export class HalfCompletedWidgetTest extends WidgetType {
+    toDOM(view: EditorView): HTMLElement {
+        const div = document.createElement("span");
+    
+        div.innerText = "👉";
+    
+        return div;
+      }
+}
--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ import {
   ViewUpdate,
   WidgetType,
 } from "@codemirror/view";
-import { EmojiWidget } from "emoji";
+import { EmojiWidget, HalfCompletedWidgetTest } from "emoji";
  
 
 // Remember to rename these classes and interfaces!
@@ -180,13 +180,24 @@ class EmojiListPlugin implements PluginValue {
   buildDecorations(view: EditorView): DecorationSet {
     const builder = new RangeSetBuilder<Decoration>();
 
+	let list_flag = false;
+
     for (let { from, to } of view.visibleRanges) {
       syntaxTree(view.state).iterate({
         from,
         to,
-        enter(node) {
-          if (node.type.name.startsWith("list")) {
+        enter(node: any) {
+			// console.log(node.type);
+			console.log(node.type.name)
+		  if (node.type.name.startsWith("formatting_formatting-list")) {
+			console.log("LIST FLAG FOUND...");
+			list_flag = true;
+			
+		  }
+
+          else if (node.type.name.startsWith("list") && list_flag) {
             // Position of the '-' or the '*'.
+			console.log("EMOJI INSERTED...");
             const listCharFrom = node.from - 2;
 
             builder.add(
@@ -196,7 +207,12 @@ class EmojiListPlugin implements PluginValue {
                 widget: new EmojiWidget(),
               })
             );
-          }
+
+			list_flag = false;
+          } else if (list_flag) {
+			console.log("LIST FLAG FALSE...")
+			list_flag = false;
+		  }
         },
       });
     }
